00b2df666d21b820cc01add7303d682a320dec6c
Trong-Thang Pham <phamtrongthang123@gmail.com>
2022-06-05 22:40:36 -0500

    change path

--- prepare_camel.sh
+++ prepare_camel.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate viser/
-# cp /media/aioz-thang/data3/aioz-thang/main_dev/lasr/DAVIS-2017-trainval-Full-Resolution/DAVIS/Annotations/Full-Resolution/camel/ -rf database/DAVIS/Annotations/Full-Resolution/
-# cp /media/aioz-thang/data3/aioz-thang/main_dev/lasr/DAVIS-2017-trainval-Full-Resolution/DAVIS/JPEGImages/Full-Resolution/camel/ -rf database/DAVIS/JPEGImages/Full-Resolution/
+mkdir -p database/DAVIS/Annotations/Full-Resolution/
+mkdir -p database/DAVIS/JPEGImages/Full-Resolution/
 
-# mkdir -p ./lasr_vcn
-# wget https://www.dropbox.com/s/bgsodsnnbxdoza3/vcn_rob.pth -O ./lasr_vcn/vcn_rob.pth
+cp /home/ptthang/DAVIS/Annotations/Full-Resolution/camel/ -rf database/DAVIS/Annotations/Full-Resolution/
+cp /home/ptthang/DAVIS/JPEGImages/Full-Resolution/camel/ -rf database/DAVIS/JPEGImages/Full-Resolution/
+
+mkdir -p ./lasr_vcn
+wget https://www.dropbox.com/s/bgsodsnnbxdoza3/vcn_rob.pth -O ./lasr_vcn/vcn_rob.pth
 
 bash preprocess/auto_gen.sh camel
