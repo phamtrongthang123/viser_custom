fa2d1060efa435ec2d76fa78b629550d130f59f4
Trong-Thang Pham <phamtrongthang123@gmail.com>
2022-06-05 21:59:27 -0500

    add correction gencode

--- third_party/softras/setup.py
+++ third_party/softras/setup.py
@@ -4,23 +4,34 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 CUDA_FLAGS = []
 
+gencodes = [
+                '-gencode', 'arch=compute_52,code=sm_52',
+                '-gencode', 'arch=compute_60,code=sm_60',
+                '-gencode', 'arch=compute_61,code=sm_61',
+                '-gencode', 'arch=compute_70,code=sm_70',
+                '-gencode', 'arch=compute_75,code=sm_75',
+                '-gencode', 'arch=compute_75,code=compute_75',]
+extra_compile_flags = {'cxx': [], 'nvcc': []}
+extra_compile_flags['nvcc'] += gencodes
+
+
 ext_modules=[
     CUDAExtension('soft_renderer.cuda.load_textures', [
         'soft_renderer/cuda/load_textures_cuda.cpp',
         'soft_renderer/cuda/load_textures_cuda_kernel.cu',
-        ]),
+        ],extra_compile_args=extra_compile_flags,),
     CUDAExtension('soft_renderer.cuda.create_texture_image', [
         'soft_renderer/cuda/create_texture_image_cuda.cpp',
         'soft_renderer/cuda/create_texture_image_cuda_kernel.cu',
-        ]),
+        ],extra_compile_args=extra_compile_flags,),
     CUDAExtension('soft_renderer.cuda.soft_rasterize', [
         'soft_renderer/cuda/soft_rasterize_cuda.cpp',
         'soft_renderer/cuda/soft_rasterize_cuda_kernel.cu',
-        ]),
+        ],extra_compile_args=extra_compile_flags,),
     CUDAExtension('soft_renderer.cuda.voxelization', [
         'soft_renderer/cuda/voxelization_cuda.cpp',
         'soft_renderer/cuda/voxelization_cuda_kernel.cu',
-        ]),
+        ],extra_compile_args=extra_compile_flags,),
     ]
 
 INSTALL_REQUIREMENTS = ['numpy', 'torch', 'torchvision', 'scikit-image', 'tqdm', 'imageio']
